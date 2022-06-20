a91ab224ca13080282559a9a52ca987e9ac7346e
lytrungkienorai <kien.lt@orai.io>
2022-06-20 17:10:40 +0700

    add: into kawaiiverse

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import cn from "classnames/bind";
 import Slider from "react-slick";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
 
 const cx = cn.bind(styles);
 
@@ -59,6 +61,20 @@ const secondSettings = {
   ],
 };
 
+const settingInto = {
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  speed: 500,
+  cssEase: "linear",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  nextArrow: <CustomNextArrow />,
+  prevArrow: <CustomPrevArrow />,
+};
+
 export default function Home() {
   const { t, i18n } = useTranslation();
 
@@ -98,6 +114,94 @@ export default function Home() {
           <div>
             <div className={cx("slider-item")}></div>
           </div>
+        </Slider>
+      </div>
+
+      <div className={cx("into-kawaiiverse")}>
+        <Slider {...settingInto}>
+          <a
+            href="https://blog.kawaii.global/introducing-kawaiiverse-subnetwork-on-oraichain-the-first-glimpse-f717d35658ff"
+            target="_blank"
+            rel="noreferrer"
+            className={cx("banner", "banner-4")}
+          >
+            <div className={cx("title")}>
+              <a
+                href="https://blog.kawaii.global/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Into the Kawaiiverse
+              </a>
+            </div>
+            <img
+              className={cx("banner-img")}
+              src={`https://miro.medium.com/max/1400/0*ATFLXLKvxGuWUlpb`}
+            />
+          </a>
+
+          <a
+            href="https://blog.kawaii.global/kawaiiverses-blooming-first-quarter-mobile-apps-new-economy-and-more-8e44d39591eb"
+            target="_blank"
+            rel="noreferrer"
+            className={cx("banner", "banner-3")}
+          >
+            <div className={cx("title")}>
+              <a
+                href="https://blog.kawaii.global/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Into the Kawaiiverse
+              </a>
+            </div>
+            <img
+              className={cx("banner-img")}
+              src={`https://miro.medium.com/max/1400/0*FGztSi3DJE07Bh95`}
+            />
+          </a>
+
+          <a
+            href="https://blog.kawaii.global/the-dawn-of-our-kawaiiverse-65567bc16ed5"
+            target="_blank"
+            rel="noreferrer"
+            className={cx("banner", "banner-2")}
+          >
+            <div className={cx("title")}>
+              <a
+                href="https://blog.kawaii.global/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Into the Kawaiiverse
+              </a>
+            </div>
+            <img
+              className={cx("banner-img")}
+              src={`https://miro.medium.com/max/1400/0*_hVANsSxLl2PWmuq`}
+            />
+          </a>
+
+          <a
+            href="https://blog.kawaii.global/kawaii-islands-2022-bouncing-into-the-anime-metaverse-9e7c1a344aac"
+            target="_blank"
+            rel="noreferrer"
+            className={cx("banner", "banner-1")}
+          >
+            <div className={cx("title")}>
+              <a
+                href="https://blog.kawaii.global/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Into the Kawaiiverse
+              </a>
+            </div>
+            <img
+              className={cx("banner-img")}
+              src={`https://miro.medium.com/max/1400/1*ZR99DqyNJUGkWc81cw4N1Q.png`}
+            />
+          </a>
         </Slider>
       </div>
     </div>
