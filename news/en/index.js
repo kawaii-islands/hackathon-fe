df8e7538a48b9874cd9e10235f3ce0b7b116b7de
duong-orai <duong.nt@orai.io>
2022-08-10 15:58:03 +0700

    art resource collection

--- news/en/index.js
+++ news/en/index.js
@@ -1,3 +1,4 @@
 import news1 from "./news-1";
+import news2 from "./news-2";
 
-export default [news1];
+export default [news2, news1];
---
+++ news/en/news-2.js
@@ -0,0 +1,190 @@
+import Link from "next/link";
+
+export default {
+  url: "kawaiiverse-art-resources-collection",
+  title: `Kawaiiverse Art Resources collection`,
+  date: "08/10/2022",
+  image: "/images/home/art-resources-collection.jpeg",
+  description:
+    "Organizing the first ever Kawaiiverse Hackathon 2022: Building the Kawaiiverse gaming ecosystem with an initial prize of $5,000 for each chosen team (no limit on the number of chosen teams - Quality is the only priority), Kawaii team also provides a collection with thousands of art resources to support participants develop their game idea.",
+  content: (
+    <>
+      <p>
+        Kawaiiverse is an Anime Metaverse co-founded in 2021 by Imba, a leading
+        game company with years of game developing experience and multiple
+        successful games including Kawaii Mansion, Kawaii Home Design,… Together
+        with the available items from the previous games, the Kawaii team has
+        been continuously rolling out new designs and new gaming items in Kawaii
+        Islands.
+      </p>
+      <p>
+        Joining the Kawaiiverse Hackathon 2022, participants will not only get
+        private advising on Gaming Development and Blockchain technology and the
+        initial prize of $5,000 but also gain access to a more valuable asset.
+        Kawaiiverse Hackathon participants can choose to develop the game with
+        their own content and art resources or opt in using the Kawaii art
+        collection. The Kawaii exclusive art resources collection contains more
+        than 5,000 items with high quality and soothing aesthetic. With multiple
+        gameplay, the Kawaii Art Collection can be divided into 4 main
+        categories: Magical Plants, Magical Creatures, Decor and Fashion. To get
+        the art sample files, go to our Hackathon page, Sample tab at:{" "}
+        <a
+          href="https://hackathon.kawaii.global/library/sample-art"
+          target="_blank"
+        >
+          https://hackathon.kawaii.global/library/sample-art
+        </a>{" "}
+        (must log in to see the files).
+      </p>
+      <h2>1. Magical Plants: </h2>
+      <p>
+        11 types of magical plants with ready to use animation throughout its
+        lifespan and matching dye potion.
+      </p>
+      <img src="/images/home/plants-collection.png" />
+      <h2>2. Magical Creatures</h2>
+      <p>
+        10 types of Magical creatures with corresponding materials. The
+        application for these creatures is only limited by your imagination.
+      </p>
+      <img src="/images/home/creatures-collection.png" />
+      <h2>3. Decor</h2>
+      <p>
+        Famous for its decoration games, the Kawaii art collection currently has
+        more than 5,000 items with different themes and growing. Creative ideas
+        can be applied to utilize these high quality items and create great
+        games.
+      </p>
+      <img src="/images/home/decors-collection.png" />
+      <h2>4. Fashion</h2>
+      <p>
+        The Kawaii Art Collection also offers various fashion items for male,
+        female and unisex characters. From the character form, appearance
+        features like face and hair to clothing in multiple styles, your game
+        characters will be fully supported. The fashion collection is growing
+        fast with over 300 items.
+      </p>
+      <img src="/images/home/fashions-collection.png" />
+      <p>
+        Hackathon participants can take advantage of these available items to
+        develop and complete their game idea. We look forward to receiving more
+        submissions combining your game ideas and our art resources. Let your
+        creativity fly high with our support!
+      </p>
+      <p>
+        In case you want to join in the Hackathon but can not find teammates or
+        want to get personal advice on both game development and blockchain
+        technology, don’t hesitate to contact us through our discord server{" "}
+        <a href="https://discord.gg/kawaiiverse" target="_blank">
+          https://discord.gg/kawaiiverse
+        </a>
+        . Start preparing your game idea now and join in our Hackathon at:{" "}
+        <a href="https://hackathon.kawaii.global" target="_blank">
+          https://hackathon.kawaii.global
+        </a>
+        .
+      </p>
+      <p>
+        Kawaiiverse Hackathon 2022 application round is opening now till Aug 20,
+        can’t wait for creative projects from everyone. In the meantime, see you
+        all in Kawaii Islands and the upcoming Kawaii Fishing Saga!
+      </p>
+      <p>
+        <b>-------------------</b>
+      </p>
+      <p>
+        <div>
+          <b>About Kawaiiverse</b>
+        </div>
+        <div>
+          Kawaiiverse (Kawaii Islands) is a collaboration between{" "}
+          <a target="_blank" href="https://imba.co/">
+            Imba Games Studio
+          </a>
+          , a leading game development studio with over 14 million customers
+          from more than 7 years of game development experience and strategic
+          partner{" "}
+          <a target="_blank" href="https://orai.io/">
+            Oraichain
+          </a>
+          , the world’s first AI-powered data oracle and AI Layer 1 in the
+          Blockchain sphere. Kawaiiverse is set to grow into an Anime Metaverse,
+          where magic mixes reality and everyone can play, create, connect and
+          earn. With the core game Kawaii Islands, Kawaiiverse is expanding
+          rapidly with its own subnetwork with low fees on Oraichain, the second
+          game Kawaii Fishing Saga and multiple exciting events lined up. Join
+          us in the adventure of the world first Anime Metaverse, Kawaiians!
+        </div>
+      </p>
+      <p>
+        <a target="_blank" href="http://kawaii.global/">
+          Website
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://twitter.com/kawaii_islands">
+          Twitter
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://t.me/kawaii_islands">
+          Telegram
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://discord.com/invite/nN4FDesACB">
+          Discord
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://blog.kawaii.global/">
+          Medium
+        </a>{" "}
+        |{" "}
+        <a
+          target="_blank"
+          href="https://www.facebook.com/kawaii.islands.official"
+        >
+          Facebook
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://bit.ly/3CPyi0x">
+          Newsletter
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://scan.kawaii.global/">
+          KawaiiScan
+        </a>
+      </p>
+      <p>
+        <b>NFTs:</b>{" "}
+        <a target="_blank" href="http://kawaii-islands.airight.io/">
+          Marketplace
+        </a>{" "}
+        |{" "}
+        <a
+          target="_blank"
+          href="https://nft-app.trava.finance/marketplace/for-rent"
+        >
+          NFT Rental
+        </a>
+      </p>
+      <p>
+        <b>Kawaii Islands:</b>{" "}
+        <a
+          target="_blank"
+          href="https://play.google.com/store/apps/details?id=co.Imba.KawaiiIslands"
+        >
+          Android
+        </a>{" "}
+        |{" "}
+        <a
+          target="_blank"
+          href="https://apps.apple.com/us/app/kawaii-islands/id1599326917"
+        >
+          iOS
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://play.kawaii.global/">
+          Web
+        </a>
+      </p>
+    </>
+  ),
+};
