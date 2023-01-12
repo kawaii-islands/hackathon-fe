43dd623ff80c9604f623988c4a43599da985adfa
lytrungkienorai <kien.lt@orai.io>
2023-01-12 16:55:38 +0700

    add: create post

--- pages/manage-posts/index.js
+++ pages/manage-posts/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "styles/manage-posts/index.module.scss";
 import cn from "classnames/bind";
-import { Button, Container } from "@mui/material";
+import { Button, Container, IconButton } from "@mui/material";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
 import Link from "next/link";
+import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 
 const cx = cn.bind(styles);
 
@@ -21,6 +24,43 @@ function ManagerPosts() {
               <AddCircleOutlineRoundedIcon /> &nbsp; Create post
             </Button>
           </Link>
+        </div>
+
+        <div className={cx("list-post")}>
+          {new Array(3).fill(1).map((_, id) => (
+            <div className={cx("post")} key={id}>
+              <div className={cx("text")}>
+                <Link
+                  href={`/library/announcing-winners-of-oraichain-hackathon-2022`}
+                >
+                  <div className={cx("title")}>
+                    Announcing Winners Of Oraichain Hackathon 2022
+                  </div>
+                </Link>
+                <div className={cx("date")}>13/06/2022</div>
+                <div className={cx("description")}>
+                  Oraichain is pleased to announce the results of the Oraichain
+                  Hackathon 2022 — our very first hackathon event that was
+                  successfully concluded yesterday in Hanoi, Vietnam!
+                </div>
+              </div>
+
+              <div className={cx("image")}>
+                <img src="https://hackathon-orai-staging.web.app/images/home/big-banner-en.png" />
+              </div>
+              <div className={cx("group-btn")}>
+                <IconButton className={cx("icon")}>
+                  <VisibilityOutlinedIcon />
+                </IconButton>
+                <IconButton className={cx("icon")}>
+                  <ModeEditOutlineOutlinedIcon />
+                </IconButton>
+                <IconButton className={cx("icon")}>
+                  <DeleteOutlineRoundedIcon />
+                </IconButton>
+              </div>
+            </div>
+          ))}
         </div>
       </Container>
     </div>
