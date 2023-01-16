eced52081c1c8f9ef6c1fc7320f70d946386149a
lytrungkienorai <kien.lt@orai.io>
2023-01-16 14:18:23 +0700

    add rules

--- news/en/index.js
+++ news/en/index.js
@@ -1,5 +1,6 @@
 import news1 from "./news-1";
 import news2 from "./news-2";
 import news3 from "./news-3";
+import news4 from './news-4';
 
-export default [news1, news2, news3];
+export default [news4, news1, news2, news3];
