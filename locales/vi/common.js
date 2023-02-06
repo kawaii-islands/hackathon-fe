a0d8c4b6a896aa922621a528d44bf0ed1ceb2a2f
lytrungkienorai <kien.lt@orai.io>
2023-02-06 17:13:15 +0700

    fix: vi in conflicts

--- locales/vi/common.js
+++ locales/vi/common.js
@@ -3,13 +3,10 @@ export default {
   "common.apply": "Nộp ý tưởng",
   "common.register": "Đăng ký",
   "common.find": "Ghép đội",
-<<<<<<< HEAD
   "common.library": "Thư viện",
   "common.verifyTitle": "Xác thực tài khoản",
   "common.verifyContent1": "Email xác thực đã được gửi đến địa chỉ",
   "common.verifyContent2": "Vui lòng kiểm tra hộp thư và xác thực tài khoản trước khi đăng nhập",
   "common.verifyButton": "Gửi lại",
-=======
   "common.managePosts": "Quản lý bài đăng",
->>>>>>> 31f116d (feat: add ckeditor)
 };
